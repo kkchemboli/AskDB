6fdea4e4aa0e58b493a997c6867a1e887210c590
kkchemboli <krishnakoundinyachemboli@Krishnas-MacBook-Pro.local>
2025-09-26 00:21:00 +0530

    removed sandbox_tool

--- tools.py
+++ tools.py
@@ -1,6 +1,4 @@
 from langchain_core.tools import tool
-from langchain_sandbox import PyodideSandboxTool
-
 def create_tools(retriever):
     """
     Create and return all custom tools given a retriever.
@@ -16,7 +14,6 @@ def create_tools(retriever):
         docs = retriever.invoke(query.lower())
         return [doc.page_content for doc in docs]
 
-    sandbox_tool = PyodideSandboxTool(allow_net=True)
 
-    # return both tools
-    return [lowercase_query_wrapper, sandbox_tool]
+    # return tools 
+    return [lowercase_query_wrapper]
